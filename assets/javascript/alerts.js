f2d085c445e285d39e9e0a51b3f9cf88e322760f
tarokeitaro <42670754+tarokeitaro@users.noreply.github.com>
2021-08-28 18:32:34 +0700

    Coding with love
    Penambahan QR Code line dengan modal image

---
+++ assets/javascript/alerts.js
@@ -0,0 +1,7 @@
+function info() {
+    alert("Anda akan meninggalkan halaman ini");
+}
+
+function ups() {
+    alert("Tidak ada yang baru saat ini. Ikuti terus informasi dari Instagram @k_tarro");
+}
