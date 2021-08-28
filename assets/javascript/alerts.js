bcfbfdaa617d4c8aabd6bb725b2ed112095bf64e
tarokeitaro <42670754+tarokeitaro@users.noreply.github.com>
2021-08-28 19:45:42 +0700

    Delete assets directory

--- assets/javascript/alerts.js
+++
@@ -1,7 +0,0 @@
-function info() {
-    alert("Anda akan meninggalkan halaman ini");
-}
-
-function ups() {
-    alert("Tidak ada yang baru saat ini. Ikuti terus informasi dari Instagram @k_tarro");
-}
