de076a8d69b8a0ff86fbab903bfdeb1fb5fb91c4
tarokeitaro <42670754+tarokeitaro@users.noreply.github.com>
2023-01-24 11:26:36 +0700

    Add files via upload

--- other/kuhaku/tips.js
+++ other/kuhaku/tips.js
@@ -1,11 +1,18 @@
 //kang acak tulisan
 function genTips() {
-  var randNum = Math.floor(Math.random() * 4) + 1;
+  var randNum = Math.floor(Math.random() * 5) + 1;
   document.getElementById('quote').innerHTML = tips[randNum];
 }
 
 //tips array
-var tips = ["Blank", "Mengklik 3x pada logo \"No Game No Life\" akan mengubah logo ke versi lain.", "Banner dengan gambar Emby dan Jellyfin dapat diklik untuk pergi langsung ke Emby web atau Jellyfin web.", "Selain anime Jepang, layanan kuhaku.id menyediakan movie Indonesia dan Internasional.", "Untuk mendaftar Jellyfin kamu bisa klik link ini : <a href=\"https://docs.google.com/forms/d/e/1FAIpQLSff_r8AdCG-D7F1gSaTBXYbWYF4vkE3YXwrsQSZt4waAIpURA/viewform\" target=\"_blank\">Daftar Jellyfin</a>."];
+var tips = [
+  "Blank",
+  "Mengklik 3x pada logo \"No Game No Life\" akan mengubah logo ke versi lain.",
+  "Banner dengan gambar Emby dan Jellyfin dapat diklik untuk pergi langsung ke Emby web atau Jellyfin web.",
+  "Selain anime Jepang, layanan kuhaku.id menyediakan movie Indonesia dan Internasional.",
+  "Untuk mendaftar Jellyfin kamu bisa klik link ini : <a href=\"https://docs.google.com/forms/d/e/1FAIpQLSff_r8AdCG-D7F1gSaTBXYbWYF4vkE3YXwrsQSZt4waAIpURA/viewform\" target=\"_blank\">Daftar Jellyfin</a>.",
+  "Layanan kuhaku.id berdiri pada hari Kamis, 24 Desember 2020, tepatnya di jam 03:09:12."
+];
 
 //box pesan
 const toastLiveExample = document.getElementById('liveToast')
@@ -26,7 +33,7 @@ function nextTips(){
 }
 setInterval(nextTips, randAppear);
 
-//notif sound
+//notif sound (aturan baru chrome https://goo.gl/xX8pDD)
 const sound = "https://tarokeitaro.github.io/other/kuhaku/notif.mp3";
 function pop(){
   new Audio(sound).play();
