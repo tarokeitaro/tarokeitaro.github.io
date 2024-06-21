ded9f267ec4d09233c1093e1a2620ede2e080a27
Keitaro Kurikuri <keitaro@msi.localhost>
2024-06-21 13:34:43 +0700

    First upload

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+AOS.init();
+
+// tanggal berakhir
+function calculateDaysRemaining(endDate) {
+    const currentDate = new Date();
+    const timeDifference = new Date(endDate) - currentDate;
+    return Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+}
+// Get element
+const dateElements = document.querySelectorAll('[data-end-date]');
+// Iteration
+dateElements.forEach(element => {
+    const endDate = element.getAttribute('data-end-date');
+    const daysRemaining = calculateDaysRemaining(endDate);
+    if (daysRemaining > 0) {
+        element.textContent = `Valid for ${daysRemaining} more days`;
+    } else {
+        element.textContent = "The validity period has ended.";
+    }
+});
+
+// footer current year
+const currentYear = new Date().getFullYear();
+// get element
+const yearElement = document.getElementById('current-year');
+// set the text
+yearElement.textContent = currentYear;
---
+++ assets/js/typewriting.js
@@ -0,0 +1,20 @@
+var typed = new Typed('#text-utama', {
+    strings: [
+        'Welcome to My Portfolio!',
+        '欢迎来到我的投资组合！',
+        '¡Bienvenido a Mi Portafolio!',
+        '私のポートフォリオへようこそ!',
+        'Bem-vindo ao Meu Portfólio!',
+        'Willkommen in meinem Portfolio!',
+        'Добро пожаловать в Мое Портфолио!',
+        'Bienvenue sur Mon Portfolio !',
+        'Selamat datang di Portofolio Saya!',
+        
+    ],
+    typeSpeed: 80,
+    backSpeed: 20,
+    backDelay: 1500,
+    smartBackspace: true,
+    shuffle: false,
+    loop: true
+  });
